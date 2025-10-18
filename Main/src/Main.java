71ee69b07b59a0e9135e061312b6a4da1f25d93b
Andrew Ralph <andrewr6467997077@gmail.com>
2025-10-17 22:36:20 -0400

    had to completely rethink my approach as i learned of Node creation methods code should run well

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    class Node{
+    static class Node{
 
         int value;
         Node next;
@@ -14,7 +14,7 @@ public class Main {
         }
     }
 
-    class singleyLinkedList{
+    static class SingleyLinkedList{
         Node head;
 
         void addNode(int value){
@@ -26,6 +26,7 @@ public class Main {
                 while(current.next != null){
                     current = current.next;
                 }
+                current.next = newNode;
             }
         }
     }
@@ -34,15 +35,36 @@ public class Main {
 
         Node current = original.head;
         while(current != null){
-            if(Integer.toString(current.value).contains("3"))
-                secondlist.add(current.value);
+            if(Integer.toString(current.value).contains("3")){
+                secondList.addNode(current.value);
+            }
+            current = current.next;
         }
-        current = current.next;
+
     }
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        SingleyLinkedList
+        SingleyLinkedList primeList = new SingleyLinkedList();
+        SingleyLinkedList secondList = new SingleyLinkedList();
+
+        System.out.println("Enter the number of nodes: ");
+        int n = scnr.nextInt();
+
+        System.out.println("Enter the numbers in the nodes: ");
+        for(int i = 0; i < n; i++){
+            primeList.addNode(scnr.nextInt());
+        }
+
+        checkForThree(primeList, secondList);
+
+        Node current = secondList.head;
+        System.out.println("The numbers in the second list containing 3 are: ");
+        while(current != null){
+            System.out.println(current.value);
+            current = current.next;
+
+        }
 
     }
 }
